923338930bae3140a62d40c6bb2f113f17ad1138
martin <martin.zivkovik@gmail.com>
2019-12-21 21:13:21 +1100

    categories

---
+++ Controllers/OurServicecs.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Venkant.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Venkant.Controllers
+{
+    public class OurService : Controller
+    {
+
+      //  private IClientsRepository _clientsRepository;
+
+        public OurService()
+        {
+
+            // this is how you create instances of depenceses 
+         //   _clientsRepository = new MockClientRepository();
+        }
+
+        
+        
+        public ViewResult ourService()
+        {
+            return View();
+        }
+        
+        public ViewResult Overheating()
+        {
+            return View();
+        }
+    }
+}
